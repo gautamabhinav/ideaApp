c1333d00784f759282537cf68091717d5c09475c
gautamabhinav <abhinavrajgautam01gmail.com>
2023-07-03 19:47:25 +0530

    connecting to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,97 @@
 const express = require('express');
-const serverConfig = require("./config/server.config")
+const serverConfig = require("./config/server.config.js");
+const mongoose = require('mongoose');
+const dbConfig = require('./config/db.config.js')
 
 
 const app = express();
 
+
+// Logic to connect to MongoDB and create an ADMIN user
+// Need to have the mongodb up and running in your local machine
+
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection ;
+
+db.on("error", ()=>{
+    console.log("Error while connecting to DB")
+});
+
+db.once("open", () => {
+    console.log("DB is connected");
+})
+
 app.listen(serverConfig.PORT, () => {
     console.log(`server started on the port number ${serverConfig.PORT}`);
 })
+
+
+
+
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+
+// const serverConfig = require('./config/server.config');
+// const dbConfig = require('./config/db.config');
+
+// const app = express();
+
+
+// mongoose.connect(dbConfig.DB_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// })
+//   .then(() => {
+//     console.log('Connected to the database');
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to the database:', error);
+//   });
+
+
+// const db = mongoose.connection;
+// db.on('error', (error) => {
+//   console.error('MongoDB connection error:', error);
+// });
+// db.once('open', () => {
+//   console.log('MongoDB connection established');
+// });
+
+// app.listen(serverConfig.PORT, () => {
+//   console.log(`Server started on port ${serverConfig.PORT}`);
+// });
+
+
+
+
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const serverConfig = require('./config/server.config');
+// const dbConfig = require('./config/db.config');
+// const app = express();
+
+// mongoose.connect(dbConfig.DB_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => {
+//     console.log('Connected to the database');
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to the database:', error);
+//   });
+
+// const db = mongoose.connection;
+// db.on('error', (error) => {
+//   console.error('MongoDB connection error:', error);
+// });
+// db.once('open', () => {
+//   console.log('MongoDB connection established');
+// });
+
+// app.listen(serverConfig.PORT, () => {
+//   console.log(`Server started on port ${serverConfig.PORT}`);
+// });
